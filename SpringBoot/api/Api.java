e840a5f95cc0c333551e7b7b554b98b9d9694c11
Chetan Rao <167339150+chetAnrAo1213@users.noreply.github.com>
2025-04-30 15:33:35 +0530

    Removed API Keys

--- SpringBoot/api/Api.java
+++ SpringBoot/api/Api.java
@@ -6,7 +6,7 @@ public class Api
 	public static final String NEWS_API = "****";
 	public static final String NEWS_URL = "https://newsdata.io/api/1/latest?apikey="+NEWS_API+"&q=";
 	public static final String TIME_API = "****";
-	public final String TIME_URL_ASIA ="https://api.ipgeolocation.io/timezone?apiKey=3fe1b01fede848bdba551e0193c5691f&lat=17.385044&long=78.486671";
+	public final String TIME_URL_ASIA ="https://api.ipgeolocation.io/timezone?apiKey=***&lat=17.385044&long=78.486671";
 	public static final String IND_LAT = "17.385044";
 	public static final String IND_LON ="78.486671";
 	public static final String timeZoneUrlForValidatingRequests ="https://api.ipgeolocation.io/timezone?apiKey="+TIME_API+"&lat="+IND_LAT+"+&long="+IND_LON;
